8bcba45b17fdc9dd3187d304e68315e576cca82a
Gonzalo <mar17004@byui.edu>
2024-03-11 10:54:25 -0300

    Completed chamber of commerce for week 2

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -4,3 +4,11 @@ document.querySelector('#lastModified').textContent = "Last Modification: " + do
 //Displays Actual Year
 const currentYear = new Date();
 document.querySelector('#year').textContent = currentYear.getFullYear();
+
+const menuButton = document.querySelector('#Hamburger');
+const menuItems = document.querySelector('nav ul');
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('open');
+    menuItems.classList.toggle('open');
+})
