4bd2d4fc91813b4edb0cfc437e060f33d3201d6f
Gonzalo <mar17004@byui.edu>
2024-04-10 15:23:19 -0400

    changes in team activity

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,27 @@ const linksURL = "https://gonzalom5.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    displayLinks(data.weeks);
   }
   
   getLinks();
+
+  const indexLinks = document.querySelector("#displayIndexLinks");
+
+  function displayLinks(allLinks) {
+    allLinks.forEach(item => {
+
+        const weekOne = document.createElement("W1");
+        weekOne.textContent = `${item.week}`;
+
+        const linksList = document.createElement("ul");
+        item.links.forEach(link => {
+            const listItem =document.createElement("li");
+            listItem.innerHTML = `<a href="${link.url}">${link.title}</a>`;
+            linksList.appendChild(listItem);
+        });
+        
+        indexLinks.appendChild(weekOne);
+        indexLinks.appendChild(linksList);
+    });
+  }
