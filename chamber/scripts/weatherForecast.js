52587d600a1694acd84d919f9e0b3a793e7261be
Gonzalo <mar17004@byui.edu>
2024-04-11 15:09:24 -0400

    changes in chamber

--- chamber/scripts/weatherForecast.js
+++ chamber/scripts/weatherForecast.js
@@ -1,45 +1,62 @@
-const chamberWeather = "https://api.openweathermap.org/data/2.5/weather?lat=-33.4569&lon=-70.64827&appid=bcc966acb1fdd4d27d55a51f8f5f65d6&units=imperial";
+const chamberWeather = "https://api.openweathermap.org/data/2.5/forecast?lat=-33.4569&lon=-70.64827&appid=bcc966acb1fdd4d27d55a51f8f5f65d6&units=imperial";
 
 fetch(chamberWeather)
   .then((response) => response.json())
   .then((allData) => {
-    forcastWeather2(allData.list)
-})
+    const threeDayForecast = getThreeDayForecast(allData.list);
+    showForecast(threeDayForecast);
+  });
 
-function forcastWeather(weatherResults) {
-  const noonWeather = weatherResults.filter(result => result.dt_txt.substring(11, 13) == '00');
-  showForecast(noonWeather)
-}
+function getThreeDayForecast(weatherResults) {
+  const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  const afterTomorrow = new Date(tomorrow);
+  afterTomorrow.setDate(afterTomorrow.getDate() + 1);
 
-function forcastWeather2(weatherResults) {
-  let noonWeather2=[]
-  weatherResults.forEach((listItem) => {
-    if (listItem.dt_txt.substring(11, 13) == '00') {
-      noonWeather2.push(listItem)
+  const threeDayForecast = {
+    today: null,
+    tomorrow: null,
+    afterTomorrow: null
+  };
+
+  weatherResults.forEach((result) => {
+    const forecastDate = new Date(result.dt_txt).getDate();
+    if (forecastDate === today.getDate() && !threeDayForecast.today) {
+      threeDayForecast.today = result;
+    } else if (forecastDate === tomorrow.getDate() && !threeDayForecast.tomorrow) {
+      threeDayForecast.tomorrow = result;
+    } else if (forecastDate === afterTomorrow.getDate() && !threeDayForecast.afterTomorrow) {
+      threeDayForecast.afterTomorrow = result;
     }
-})
-  showForecast(noonWeather2)
+  });
+
+  return threeDayForecast;
 }
 
-function showForecast(chamberForecast) {  
-  const forecast = document.querySelector('#forecast')
-  chamberForecast.forEach((day) => {
-    const cityTemp=document.createElement('p')
-    cityTemp.className="temp"
-    cityTemp.textContent = Math.floor(day.main.temp) + "°"
+function showForecast(threeDayForecast) {  
+  const forecast = document.querySelector('#forecast');
+  forecast.innerHTML = '';
 
-    const chamberDesc=document.createElement('p')
-    chamberDesc.className='desc'
-    chamberDesc.textContent = day.weather[0].description
-    
-    const chamberIcon = document.createElement('img')
-    chamberIcon.src = `//openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`
-    chamberIcon.alt = day.weather[0].description
+  Object.values(threeDayForecast).forEach((day) => {
+    if (day) {
+      const cityTemp = document.createElement('p');
+      cityTemp.className = "temp";
+      cityTemp.textContent = Math.floor(day.main.temp) + "°";
 
-    const theWeather = document.createElement('div')
-    theWeather.appendChild(cityTemp)
-    theWeather.appendChild(chamberDesc)
-    theWeather.appendChild(chamberIcon)
-    forecast.appendChild(theWeather)
+      const chamberDesc = document.createElement('p');
+      chamberDesc.className = 'desc';
+      chamberDesc.textContent = day.weather[0].description;
+
+      const chamberIcon = document.createElement('img');
+      chamberIcon.src = `//openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
+      chamberIcon.alt = day.weather[0].description;
+
+      const theWeather = document.createElement('div');
+      theWeather.appendChild(cityTemp);
+      theWeather.appendChild(chamberDesc);
+      theWeather.appendChild(chamberIcon);
+      forecast.appendChild(theWeather);
+    }
   });
 }
